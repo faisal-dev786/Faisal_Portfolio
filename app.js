a121c6880e6585838ef8cd61272c93ec619fcd60
Faisal <fasinadem108@gmail.com>
2023-01-24 14:36:20 +0500

    a

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+  // Navbar
+  let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
+
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("active");
+});
+
+// 
+
